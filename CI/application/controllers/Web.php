a0382dbcf718c21bfef5b2a6627b00c2e3bf27d5
CnineWidom <2779386157@qq.com>
2019-05-08 17:09:13 +0800

    详情和讨论

--- CI/application/controllers/Web.php
+++ CI/application/controllers/Web.php
@@ -213,6 +213,7 @@ class Web extends CI_Controller {
 		echo json_encode($allCount);
 	}
 
+
 	public function getCritic(){
 		$pageSize = 2;
 		$typeArr = [
@@ -226,17 +227,32 @@ class Web extends CI_Controller {
 		$type = (int)$this->input->post('type');
 		if($type){
 			$countSql = "SELECT count(*) as allCount FROM `case` WHERE `type` = $type";
+			$allCount = $this->db->query($countSql)->result();
+			$pages = ($page-1)*$pageSize;
+			$sql = "select * from `case` where  `type` = $type order by `createtime` desc limit $pages,$pageSize";
+			$res = $this->db->query($sql)->result();
+			foreach ($res as $key => &$value) {
+				$value->createtime = date('Y-m-d',$value->createtime);
+				$title = explode(' ', $value->caseName);
+				if(sizeof($title) > 1){
+					$value->title = $title[0];
+					$value->title1 = $title[1];
+				}else {
+					$value->title1 = $title[0];
+					$value->title = '';
+				}	
+			}
+			$countArr[$value->type]['result'] = $res;
+			$countArr[$type]['allCount'] = $allCount[0]->allCount;
+			$countArr[$type]['pageSize'] = $pageSize;
+			$countArr[$type]['page'] = $page;
+			$countArr[$type]['pageCount'] = ceil($allCount[0]->allCount/$pageSize);
+			$countArr[$type]['typeTmp'] = $typeArr[$type];
+			$countArr[$type]['type'] = $type;
 		}else {
 			$countSql = "SELECT count(*) as allCount,type FROM `case`  group by type";
 			$allCount = $this->db->query($countSql)->result();
 			foreach ($allCount as $key => &$value) {
-				$countArr[$key]['pageSize'] = $pageSize;
-				$countArr[$key]['page'] = $page;
-				$countArr[$key]['allCount'] = $value->allCount;
-				
-				$countArr[$key]['pageCount'] = ceil($value->allCount/$pageSize);
-				$countArr[$key]['typeTmp'] = $typeArr[$value->type];
-				$countArr[$key]['type'] = $value->type;
 				$sql = "select * from `case` where  `type` = $value->type order by `createtime` desc limit $pageSize";
 				$res = $this->db->query($sql)->result();
 				foreach ($res as $k => &$val) {
@@ -248,21 +264,110 @@ class Web extends CI_Controller {
 					}else {
 						$val->title1 = $title[0];
 						$val->title = '';
-					}
-					
+					}	
 				}
-				$countArr[$key]['result'] = $res;
+				$countArr[$value->type]['result'] = $res;
+				$countArr[$value->type]['pageSize'] = $pageSize;
+				$countArr[$value->type]['page'] = $page;
+				$countArr[$value->type]['allCount'] = $value->allCount;
+				$countArr[$value->type]['pageCount'] = ceil($value->allCount/$pageSize);
+				$countArr[$value->type]['typeTmp'] = $typeArr[$value->type];
+				$countArr[$value->type]['type'] = $value->type;
 				unset($val);
 			}
 			unset($value);
 
 		}
-		
-
 		echo json_encode($countArr);
-		
-		// $pageCount = ceil($allCount[0]->allCount/$pageSize);//页数
-		// $sql = "select * from `critic` where  `type` = $type order by `createtime` desc limit $pages,$pageSize";
+	}
 
+	public function getComment(){
+		$id = (int)$this->input->post('id');
+		if($id){
+			$time =time();
+			$userName = $this->input->post('username');
+			$userSql = "select * from `usermsg` where userName = $userName";
+			$userMess = $this->db->query($userSql)->result();
+			if($userMess){
+				$userId = $userMess[0]->userID;
+				//插入阅读表 先检查一下阅读的的时间大于多少
+				$checkSql = "select * from `read` where `userID` = $userId and `inform_ID` = $id order by `createtime` desc limit 1";
+				$checkMess = $this->db->query($checkSql)->result();
+				$readData =[
+					'userID' => $userId,
+					'createtime' => $time,
+					'inform_ID' => $id
+				];
+				if($checkMess){
+					$timeInter = $time - $checkMess[0]->createtime; //时间间隔
+					if( $timeInter > 300 ){
+						$str = $this->db->insert_string('read',$readData);
+						$query = $this->db->query($str);
+					}
+				}else{
+					$str = $this->db->insert_string('read',$readData);
+					$query = $this->db->query($str);
+				}
+				
+				$sql = "select * from `case` where caseID = $id  "; 
+				$res = $this->db->query($sql)->result();
+				if($res){
+					foreach ($res as $key => &$value) {
+						$commentSql = "select c.*,u.userName from `critic` as c left join `usermsg` as u on c.userID = u.userID  where `caseID` = $value->caseID";
+						$commentRes = $this->db->query($commentSql)->result();
+						foreach ($commentRes as $k => &$v) {
+							$v->createtime = date('Y-m-d',$v->createtime);
+							$v->userName = empty($v->userName) ? "****":$v->userName; 
+						}
+						$readSql = "select count(*)as readCount from `read` where `inform_ID` = $id";
+						$readCount = $this->db->query($readSql)->result();
+						if($readCount){
+							$value->readCount = $readCount[0]->readCount;
+						}
+						else $value->readCount = 0;
+						$value->commentMess = $commentRes;
+						$value->createtime = date('Y-m-d',$value->createtime);
+						$value->commentCount = sizeof($commentRes);
+					}
+					
+				}else{
+					$res = false;
+				}
+			}else{
+				$res = false;
+			}
+			echo json_encode($res);
+		}
+	}
+
+	public function sendMess()
+	{
+		$id = $this->input->post('id');
+		$userName = $this->input->post('userName');
+		$critic_info = $this->input->post('critic_info');
+		$time = time();
+		$userSql = "select * from `usermsg` where userName = $userName";
+		$userMess = $this->db->query($userSql)->result(); 
+		if($userMess){
+			$userID = $userMess[0]->userID;
+			$sendData = [
+				'createtime'=>$time,
+				'critic_info' =>$critic_info,
+				'userID' => $userID,
+				'caseID' => $id,
+			];
+			$str = $this->db->insert_string('critic',$sendData);
+			$query = $this->db->query($str);
+			$msg = [
+				'code' =>200,
+				'msg' =>'发布成功'
+			];
+		}else{
+			$msg =[
+				'code' => -1,
+				'msg' => '没有该用户'
+			];
+		}
+		echo json_encode($msg);
 	}
 }
